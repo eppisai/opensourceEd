f103fe57ce9c1adab9736dcfcca592a22a46d513
Aman <54789531+eppisai@users.noreply.github.com>
2021-03-12 22:03:34 +0530

    making videoplayer without newwindow

--- prot1_problem/src/components/VideoScreen.js
+++ prot1_problem/src/components/VideoScreen.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import Editor from './Editor'
-import useLocalStorage from '../hooks/useLocalStorage'
 
 function App(props) {
-  console.log("video: ",props.rec);
-  const [html, setHtml] = useLocalStorage('html', '')
-  const [css, setCss] = useLocalStorage('css', '')
-  const [js, setJs] = useLocalStorage('js', '')
-  const [srcDoc, setSrcDoc] = useState('')
 
+  console.log("video: ",props.rec);
+  const [html, setHtml] = useState('')
+  const [css, setCss] = useState('')
+  const [js, setJs] = useState('')
+  const [srcDoc, setSrcDoc] = useState('')
+  const rec = props.rec;
   useEffect(() => {
     const timeout = setTimeout(() => {
       setSrcDoc(`
@@ -22,7 +22,73 @@ function App(props) {
 
     return () => clearTimeout(timeout)
   }, [html, css, js])
+  
+  useEffect(() => {
+    var cur_prog = 0,html,css,js;
+    const fakeCursor = document.createElement('div');
+    var play = document.getElementById("pa");
+    let i = 0;
+    play.addEventListener("click",function(){
+      var cursor = document.getElementsByClassName("cursor");
+      for(i = 0; i < cursor.length; i++){
+                    cursor[i].remove();
+      }
+      const fakeCursor = document.createElement('div');
+                  fakeCursor.className = "cursor";
+                  document.body.appendChild(fakeCursor);
+                  const startPlay = Date.now();
+      })
+      var doc = document.documentElement;
+      const startPlay = Date.now();
+      (function draw() {
+        if(cur_prog > 0){
+          i = cur_prog;
+          cur_prog = 0
+          var h = document.getElementById("htmltext");
+          setHtml(html);
+          var c = document.getElementById("csstext");
+          setCss(css);
+          var j = document.getElementById("jstext");
+          setJs(js);
+        }
+        let event = rec.events[i];
+        if (!event) {
+          return;
+        }
+        let offsetRecording = event.time - rec.startTime;
+        let offsetPlay = (Date.now() - startPlay) * 1;
+        if (offsetPlay >= offsetRecording) {
+          drawEvent(event, fakeCursor, doc);
+          i++;
+        }        
+      })();
 
+      function drawEvent(event, fakeCursor, Doc) {
+        if (event.type === "click" || event.type === "mousemove") {
+            console.log("mouse");
+            fakeCursor.style.top = event.y
+            fakeCursor.style.left = event.x  
+        }
+        if (event.type === "click") {
+          console.log("mouseclick");
+          flashClass(fakeCursor, "click");
+          console.log(event.target);
+          var tar = document.getElementById(event.target);
+          flashClass(tar, "clicked");
+        }
+        if (event.type === "keypress") {
+          console.log("keypress");
+          const path = event.target;
+          var tar = document.getElementById(path);
+          tar.focus();
+          tar.value = event.value;
+        } 
+      }
+      function flashClass(el, className) {
+        el.classList.add(className);
+        setTimeout(function(){ el.classList.remove(className); }, 200);
+      }
+  })
   return (
     <>
       <div className="pane top-pane">
@@ -55,6 +121,7 @@ function App(props) {
           height="100%"
         />
       </div>
+      <button id="pa">play</button>
     </>
   )
 }
