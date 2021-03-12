e305ff4b7d3c5b3838548d09c43b67faba73b990
Aman <54789531+eppisai@users.noreply.github.com>
2021-03-12 10:59:30 +0530

    audio problem not changing state

---
+++ prot1_problem/src/components/App.js
@@ -0,0 +1,238 @@
+import React, { useState, useEffect } from 'react';
+import Editor from './Editor'
+import useLocalStorage from '../hooks/useLocalStorage'
+import MicRecorder from 'mic-recorder-to-mp3';
+
+function App() {
+  const Mp3Recorder = new MicRecorder({ bitRate: 128 });
+  const [html, setHtml] = useLocalStorage('html', '')
+  const [css, setCss] = useLocalStorage('css', '')
+  const [js, setJs] = useLocalStorage('js', '')
+  const [srcDoc, setSrcDoc] = useState('')
+  const [Recording,setRecording] = useState({ events: [], startTime: -1 }); 
+  const [Audio,setAudio] = useState({ isRecording: false,blobURL: '',isBlocked: false,})
+  // Record each type of event
+  const handlers = [
+      {
+        eventName: "mousemove",
+        handler: function handleMouseMove(e) {
+          Recording.events.push({
+            type: "mousemove",
+            x: e.pageX,
+            y: e.pageY,
+            time: Date.now()
+          });
+        }
+      },
+      {
+        eventName: "click",
+        handler: function handleClick(e) {
+          Recording.events.push({
+            type: "click",
+            target: e.target.id,
+            x: e.pageX,
+            y: e.pageY,
+            time: Date.now()
+          });
+        }
+      },
+      {
+        eventName: "keypress",
+        handler: function handleKeyPress(e) {
+          Recording.events.push({
+            type: "keypress",
+            target: e.target.id,
+            value: e.target.value,
+            keyCode: e.keyCode,
+            time: Date.now()
+          });
+        }
+      }
+  ];
+  function listen(eventName, handler) {
+      return document.documentElement.addEventListener(eventName, handler, true);
+    }
+   
+  function removeListener(eventName, handler) {
+       return document.documentElement.removeEventListener(
+         eventName,
+         handler,
+         true
+       );
+  }
+  function startRecording() {
+
+    Recording.startTime = Date.now();
+    Recording.events = [];
+    handlers.map(x => listen(x.eventName, x.handler));
+
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    startRecording();
+    console.log('The Button was clicked.');
+
+  }
+  function stopRecording() {
+    // stop recording
+  
+    handlers.map(x => removeListener(x.eventName, x.handler));
+    console.log(Recording);
+  }
+  function handleStop(e) {
+    e.preventDefault();
+    stopRecording();
+    console.log('Recording Stopped.');
+
+  }
+  function start() {
+    if (Audio.isBlocked) {
+      console.log('Permission Denied');
+    } else {
+      var A = Audio;
+      A.isRecording = true;
+      setAudio(A);
+      Mp3Recorder
+        .start()
+        .then(() => {
+          var A = Audio;
+          A.isRecording = true;
+          setAudio(A);
+        }).catch((e) => console.error(e));
+    }
+  };
+  function stop(){
+    var A = Audio;
+    A.isRecording = true;
+    setAudio(A);
+    Mp3Recorder
+      .stop()
+      .getMp3()
+      .then(([buffer, babu]) => {
+        const blob = URL.createObjectURL(babu)
+        var A = Audio;
+        A.isRecording = false;
+        A.blobURL = blob;
+        setAudio(A);
+      }).catch((e) => console.log(e));
+      console.log("working:", Audio.blobURL);
+  };  
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `)
+    }, 250)
+
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
+
+  useEffect(() => {
+    navigator.getUserMedia({ audio: true },
+      () => {
+        console.log('Permission Granted');
+        var A = Audio;
+        A.isBlocked = false;
+        setAudio(A);
+      },
+      () => {
+        var A = Audio;
+        A.isBlocked = true;
+        setAudio(A)
+      },
+    );
+    // return () => {
+    //   cleanup
+    // }
+  }, [Audio])
+
+  useEffect(() => {
+   
+    const handlers = [
+      {
+        eventName: "mousemove",
+        handler: function handleMouseMove(e) {
+          Recording.events.push({
+            type: "mousemove",
+            x: e.pageX,
+            y: e.pageY,
+            time: Date.now()
+          });
+        }
+      },
+      {
+        eventName: "click",
+        handler: function handleClick(e) {
+          Recording.events.push({
+            type: "click",
+            target: e.target.id,
+            x: e.pageX,
+            y: e.pageY,
+            time: Date.now()
+          });
+        }
+      },
+      {
+        eventName: "keypress",
+        handler: function handleKeyPress(e) {
+          Recording.events.push({
+            type: "keypress",
+            target: e.target.id,
+            value: e.target.value,
+            keyCode: e.keyCode,
+            time: Date.now()
+          });
+        }
+      }
+    ];
+  })
+
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+      <button onClick={handleClick} className = "button" id="record"><img id="record_img" src="https://img.icons8.com/dusk/64/000000/record.png"/></button>
+      <button onClick={handleStop} className = "button" id="record">Stop Recording</button>
+      
+<button onClick={start} disabled={Audio.isRecording}>Record Audio</button>
+<button onClick={stop}  >Stop Audio</button>
+{console.log(Audio.blobURL)}
+<audio src={Audio.blobURL} controls="controls" />
+    </>
+  )
+}
+
+export default App;
